ae2f308ff8d463040e1e274f96054a2292063c5f
Jochen Zehnder <mail@jochenzehnder.com>
2013-05-21 17:37:16 +0200

    Solution for exercise 11 including assignment of tasks and tests

---
+++ test/serie11/MainTest.java
@@ -0,0 +1,12 @@
+package serie11;
+
+import org.junit.Test;
+
+import utils.TestUtils;
+
+public class MainTest {
+	@Test
+	public void publicTest() throws Exception {
+		TestUtils.executeTest("public.in", "public.out", MainTest.class, Main.class);
+	}
+}
